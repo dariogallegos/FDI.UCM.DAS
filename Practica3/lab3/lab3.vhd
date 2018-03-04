c3beb07749fc8d1d2f501e7ef0f80a187c99c975
Dario Gallegos <dariogal@ucm.es>
2018-03-04 20:54:41 +0100

    Cerradura eletronica
    Terminamos la maquina de estados.
    Se corrige los fallos en los casting.
    Se realizan purebas para asegurar el correcto funcionamiento.

--- Practica3/lab3/lab3.vhd
+++ Practica3/lab3/lab3.vhd
@@ -54,12 +54,12 @@ begin
   
   clkGenerator : frequencySynthesizer
     generic map ( FREQ => 50_000, MODE => "LOW", MULTIPLY => 3, DIVIDE => 5 )
-    port map ( clkIn => osc,ready =>ready , clkOut => clk);
+    port map ( clkIn => osc, ready =>ready , clkOut => clk);
 
   ------------------
 
   enterSynchronizer : synchronizer
-    generic map ( STAGES => 2, INIT => 1 )
+    generic map ( STAGES => 2, INIT => '1' )
     port map ( rst_n => rst_n, clk => clk, x => enter_n ,xSync => enterSync_n);
    
   enterDebouncer : debouncer
@@ -74,8 +74,8 @@ begin
   for i in switches_n'range generate
   begin
     switchsynchronizer : synchronizer
-      generic map ( STAGES => 2, INIT => 1 )
-      port map ( rst_n => rst_n, clk => clk, x => switches_n  ,xSync => switchesSync_n );
+      generic map ( STAGES => 2, INIT => '1' )
+      port map ( rst_n => rst_n, clk => clk, x => switches_n(i)  ,xSync => switchesSync_n(i) );
   end generate;
 
   ------------------
@@ -85,44 +85,89 @@ begin
     type states is (initial, S3, S2, S1, S0); 
     variable state: states;
   begin 
-    if ... then
-      ldCode <= ...;
-    ...
-    end if;
+	 if state = initial and enterfall = '1' then
+		 ldCode <= '1';
+	 else
+		 ldCode <= '0';
+	 end if;
+	 --Calculamos las salidas en funcion del estado
     case state is
-      when initial =>
-        tries <= ...;
-        lock  <= ...;
-    ...
+		when initial =>
+        tries <= "1010";
+        lock  <= '0';
+		when S3 =>
+			lock <= '1';
+			tries <= "0011";
+		when S2 =>
+			lock <= '1';
+			tries <= "0010";
+		when S1 =>
+			lock <= '1';
+			tries <= "0001";
+		when S0 =>
+			lock <= '1';
+			tries <= "1100";
     end case;
 	 ----- arriba logia de estados y abajo la secuencial
     if rst_n='0' then
-      state := ...;
+      state := initial;
     elsif rising_edge(clk) then
       case state is
         when initial =>
-        ...
+				 if enterfall = '1' then
+					 state := S3;
+				 else 
+					 state := initial;
+				 end if;
+		  when S3 => 
+				 if enterfall = '1' and eq = '1' then
+					 state := initial;
+				 elsif enterfall = '1' and eq = '0' then
+					 state := S2;
+				 else 
+					 state := S3;
+				 end if;
+		  when S2 => 
+			    if enterfall = '1' and eq = '1' then
+					 state := initial;
+			    elsif enterfall = '1' and eq = '0' then
+					 state := S1;
+				 else 
+					 state := S2;
+				 end if;
+		  when S1 => 
+				 if enterfall = '1' and eq = '1'  then
+					 state := initial;
+				 elsif enterfall = '1' and eq = '0' then
+					 state := S0;
+				 else 
+					 state := S1;
+				 end if;	
+			when S0 =>
+				 state := S0;
       end case;
     end if;
   end process;  
-
+  
+  
   codeRegister :
   process (rst_n, clk)
   begin
-	code <= (others=>'0');
 	if rst_n = '0' then 
 		code <=(others=>'0');
 	elsif rising_edge(clk) then
 		if ldCode = '1' then
 			code <= switchesSync_n;
+		end if;
+	end if;
   end process;
   
   comparator:
   eq <= '1' when (switchesSync_n = code ) else '0'; -- comparador '=' con un multiplexor 
     
   rigthConverter : bin2segs 
-    port map( bin => tries , dp => '0', segs =>upSegs);
+  port map( bin => tries , dp => '0', segs => upSegs);
   
-  leds <= std_logic_vector((others =>'0')&lock); -- si no meter 7 ceros antes del lock "0000000"
+  leds <= ("0000000" & lock); -- si no meter 7 ceros antes del lock "0000000"
 
 end syn;
